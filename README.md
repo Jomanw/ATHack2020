9bfe7089d824e839b8e75f9e60dc46b7bbe9b959
Jordan Wick <wickj@mit.edu>
2020-02-21 16:04:50 -0500

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ATHack2020
+
+### Instructions
+To open a window that accesses your webcam, run the following command:
+`python3 video_window.py`
---
+++ video_window.py
@@ -0,0 +1,67 @@
+from PySide2.QtCore import *
+from PySide2.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QVBoxLayout
+from PySide2.QtGui import *
+import cv2
+import numpy as np
+import sys
+
+class MainApp(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+        # self.video_size = QSize(320, 240)
+        self.video_size = QSize(640, 480)
+        self.setup_ui()
+        self.setup_camera()
+
+    def setup_ui(self):
+        """Initialize widgets.
+        """
+        self.image_label = QLabel()
+        self.image_label.setFixedSize(self.video_size)
+
+        self.quit_button = QPushButton("Quit")
+        self.quit_button.clicked.connect(self.close)
+
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addWidget(self.image_label)
+        self.main_layout.addWidget(self.quit_button)
+
+        self.setLayout(self.main_layout)
+
+    def setup_camera(self):
+        """Initialize camera.
+        """
+        self.capture = cv2.VideoCapture(0)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.video_size.width())
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.video_size.height())
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.display_video_stream)
+        self.timer.start(30)
+
+    def display_video_stream(self):
+        """Read frame from camera and repaint QLabel widget.
+        """
+        _, frame = self.capture.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.flip(frame, 1)
+
+        # Can do whatever we want to the image here
+
+        def adjust_gamma(image, gamma=1.0):
+            print(image)
+            invGamma = 1.0 / gamma
+            table = np.array(np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8"))
+            return cv2.LUT(image, table)
+
+        frame = adjust_gamma(frame, 5.0)
+        image = QImage(frame, frame.shape[1], frame.shape[0],
+                       frame.strides[0], QImage.Format_RGB888)
+        self.image_label.setPixmap(QPixmap.fromImage(image))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainApp()
+    win.show()
+    sys.exit(app.exec_())
